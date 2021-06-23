0d59251aca2d18bf10be9aa6627aa6194ae6fea1
Carol Brandes <carolbrandes23@gmail.com>
2021-06-23 19:58:08 -0300

    "ajustei" erro ts na pag Home voltando ao utilizar no jeito antigo sem usar custom Hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,16 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 import { useHistory } from "react-router-dom";
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 import { Button } from "../components/Button";
-import { useAuth } from "../hooks/useAuth";
 
 import "../styles/auth.scss";
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useAuth;
+  const { user, signInWithGoogle } = useContext(AuthContext);
 
   async function handleCreateRoom() {
     if (!user) {

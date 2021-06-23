cd3d6db0e731694b4a309d1300cf81c2607bf211
Carol Brandes <carolbrandes23@gmail.com>
2021-06-23 19:33:17 -0300

    erro ts pag home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { NewRoom } from "./pages/NewRoom";
+import { Home } from "./pages/Home";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
